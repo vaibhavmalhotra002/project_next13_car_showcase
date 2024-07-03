5eac33e5277a59dcd2b3e22872326b765ac544c5
Vaibhav Malhotra <76607940+vaibhavmalhotra002@users.noreply.github.com>
2024-07-03 21:53:06 +0530

    Update tailwind.config.js
    Updated changes :- 002vaibhav

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,14 +12,14 @@ module.exports = {
         inter: ["Inter", "sans-serif"],
       },
       colors: {
-        "black-100": "#2B2C35",
+        "black-100": "#2BCD35",
         "primary-blue": {
           DEFAULT: "#2B59FF",
           100: "#F5F8FF",
         },
         "secondary-orange": "#f79761",
         "light-white": {
-          DEFAULT: "rgba(59,60,152,0.03)",
+          DEFAULT: "rgba(59,60,178,0.03)",
           100: "rgba(59,60,152,0.02)",
         },
         grey: "#747A88",
